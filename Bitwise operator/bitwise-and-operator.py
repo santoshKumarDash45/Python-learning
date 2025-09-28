8643a3ff177b8644c5d76d8f94c7426480fe7030
santoshKumarDash45 <santoshkumardash909@gmail.com>
2025-09-28 18:44:41 +0530

    code pushed for bitwise and or and xor operator

---
+++ Bitwise operator/bitwise-and-operator.py
@@ -0,0 +1,25 @@
+# Bitwise and operator can be applied to only int and bool type
+
+print(10 & 20) 
+
+# Answer will be 0
+
+# print(10.5 & 20.6)
+
+# Type error because both are float 
+
+# print('durge' & 'soft')
+
+# Type error because both are string values
+
+print(4 & 5)
+
+# Answer is 4 as if both bits are 1 then result is 1 or else 0
+
+print(4 | 5)
+
+# Answer is 5 as if atleast one bit is 1 then result is 1 or else 0
+
+print(4 ^ 5) 
+
+# Answer is 1 as if both bits are different then result is 1 or else 0
