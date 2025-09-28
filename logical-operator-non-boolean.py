edae925c6162b77208a1ef70ec41a6415f614617
santoshKumarDash45 <santoshkumardash909@gmail.com>
2025-09-28 10:59:56 +0530

    code pushed for first program in logican operator

---
+++ logical-operator-non-boolean.py
@@ -0,0 +1,3 @@
+print(10 and 20)
+
+# Here answer will be 20 as 10 is non-zero so according to and operator if x is True then answer will be y and if x is false means x is zero then answer will be false
