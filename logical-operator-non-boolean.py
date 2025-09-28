d8ea148843ef464c8c4bad54829942c9be245719
santoshKumarDash45 <santoshkumardash909@gmail.com>
2025-09-28 11:29:59 +0530

    code pushed for logic operator non boolean type

--- logical-operator-non-boolean.py
+++ logical-operator-non-boolean.py
@@ -1,3 +1,14 @@
 print(10 and 20)
 
-# Here answer will be 20 as 10 is non-zero so according to and operator if x is True then answer will be y and if x is false means x is zero then answer will be false
+# Here answer will be 20 as 10 is non-zero so according to and operator 
+# if x is True then answer will be y and 
+# if x is false means x is zero then answer will be false
+
+print(0 and 20)
+
+# Here answer will be false then answer will be o
+
+print('' and 'santosh')
+
+# Here answer will be empty string as here first parameter is empty string means False then 
+# in and operator if x is false then answer will be false
