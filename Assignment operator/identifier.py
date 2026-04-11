4ecf0e3d9c6db5581751ec66cda561e3b62b0462
santoshKumarDash45 <santoshkumardash909@gmail.com>
2026-04-11 12:51:51 +0530

    Pushed code for identifier

---
+++ Assignment operator/identifier.py
@@ -0,0 +1,38 @@
+# A name in the python program is called as identifier. It can be a variable name ,
+# a class name or a method name . This is basically used for identity purpose .
+
+
+# Rules:-
+#  1 - It can be between a-z , A-Z , 0-9 , _(underscore).
+#  2 - It must not start with digit.
+#  3 - case sensitive .
+#  4 - We cannot use reserved keywords as like if, if elif, etc.
+#  5 - We cannot use space .
+#  6 - Special symbols are not allowed .
+#  7 - No length limit for identifier.
+#  8 - If a variable is declared as normal variable then it is valid.
+#  9 - If a variable is declared as protected variable then it should be started with _(underscore).
+#  10 - If a variable is declared as private variable then it should be started with __(double underscore).
+#  11 - If a variable is declared as magic variable then it should starts with double __(underscore) and ends with double__(underscore).
+
+ 
+
+#  Example---
+
+x = 10
+print(x)
+
+_x = 20
+print(_x)
+
+__x = 30
+print(__x)
+
+if = 30
+print(if)  # here if is reserved keyword so it will show error .
+
+
+
+
+
+
